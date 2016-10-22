f94a390271c053d38cf5cc5d492a4cfd1e8bbdc5
pkdhirasaria <pkdhirasaria@gmail.com>
2016-10-22 21:41:28 +0530

    Add files via upload

---
+++ FIFO/server.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<string.h>
+int main()
+{
+ char fname[25]="";
+ char fcontent[100]="";
+ int fd,fd1,fd2;
+ mkfifo("fifo1",0600);
+ mkfifo("fifo2",0600);
+ fd=open("fifo1",O_RDONLY);
+ fd1=open("fifo2",O_WRONLY);
+ read(fd,fname,25);
+ fd2=open(fname,O_RDONLY);
+ while(read(fd2,fcontent,100)!=0)
+ {
+ printf("%s\n",fcontent);
+ if(fd<0)
+ write(fd1,"file not exit",14);
+ else
+ write(fd1,fcontent,strlen(fcontent));
+ }
+ close(fd);
+ close(fd1);
+ close(fd2);
+}
