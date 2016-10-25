d90f07cd4f2c625a3a116458c17acde47b03f7e6
pkdhirasaria <pkdhirasaria@gmail.com>
2016-10-25 16:23:22 +0530

    Create 1.tcl

---
+++ Part-A/1.tcl
@@ -0,0 +1,90 @@
+//Use command ns 1.tcl to execute
+// and grep -c "^d" out.tr total number of pakcet drop 
+
+
+# This script is created by NSG2 beta1
+# <http://wushoupong.googlepages.com/nsg>
+
+#===================================
+#     Simulation parameters setup
+#===================================
+set val(stop)   10.0                         ;# time of simulation end
+
+#===================================
+#        Initialization        
+#===================================
+#Create a ns simulator
+set ns [new Simulator]
+
+#Open the NS trace file
+set tracefile [open out.tr w]
+$ns trace-all $tracefile
+
+#Open the NAM trace file
+set namfile [open out.nam w]
+$ns namtrace-all $namfile
+
+#===================================
+#        Nodes Definition        
+#===================================
+#Create 3 nodes
+set n0 [$ns node]
+set n1 [$ns node]
+set n2 [$ns node]
+
+#===================================
+#        Links Definition        
+#===================================
+#Createlinks between nodes
+$ns duplex-link $n0 $n1 1.0Mb 10ms DropTail
+$ns queue-limit $n0 $n1 10
+$ns duplex-link $n1 $n2 1.0Mb 10ms DropTail
+$ns queue-limit $n1 $n2 10
+
+#Give node position (for NAM)
+$ns duplex-link-op $n1 $n0 orient left
+$ns duplex-link-op $n1 $n2 orient left-down
+
+#===================================
+#        Agents Definition        
+#===================================
+#Setup a TCP connection
+set tcp0 [new Agent/TCP]
+$ns attach-agent $n0 $tcp0
+set sink1 [new Agent/TCPSink]
+$ns attach-agent $n2 $sink1
+$ns connect $tcp0 $sink1
+$tcp0 set packetSize_ 2500
+
+
+#===================================
+#        Applications Definition        
+#===================================
+#Setup a CBR Application over TCP connection
+set cbr0 [new Application/Traffic/CBR]
+$cbr0 attach-agent $tcp0
+$cbr0 set packetSize_ 100
+#$cbr0 set rate_ 1.0Mb
+#$cbr0 set random_ null
+
+$cbr0 set interval_ .005
+$ns at 1.0 "$cbr0 start"
+$ns at 5.0 "$cbr0 stop"
+
+
+#===================================
+#        Termination        
+#===================================
+#Define a 'finish' procedure
+proc finish {} {
+    global ns tracefile namfile
+    $ns flush-trace
+    close $tracefile
+    close $namfile
+    exec nam out.nam &
+    exit 0
+}
+$ns at $val(stop) "$ns nam-end-wireless $val(stop)"
+$ns at $val(stop) "finish"
+$ns at $val(stop) "puts \"done\" ; $ns halt"
+$ns run
