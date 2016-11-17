0bc91ef4d80f8a6d2f30ce0fab274ac37cae5393
pkdhirasaria <pkdhirasaria@gmail.com>
2016-11-18 02:32:37 +0530

    Update frame.cpp

--- frame.cpp
+++ frame.cpp
@@ -1,24 +1,16 @@
+//Use long message for better random shuffling
 #include <bits/stdc++.h>
 using namespace std;
-#define fr(a,b) for(i=a;i<b;i++)
-#define rep(a,b) for(i=a;i>=b;i--)
-#define f first
-#define s second
-#define inp(a,n) fr(i,n) cin>>a[i]
-#define print(a,n) fr(i,n) {cout << a[i] << " ";} cout << endl
-#define ll long long
-#define my(type) (char *)(&type+1)-(char *)(&type)
 int main()
 {
     int i=0;
     srand(time(NULL));
     cout<<"\nEnter the message :";
-    string s;//="hello how are you i hope you are fine";
+    string s;
     getline(cin,s);
     vector<string>v;
-    int k=0;
-    std::istringstream iss(s);
-    for(string s;iss>>s;)
+    std::istringstream iss(s);// built in function for breaking the string on spaces
+    for(string s;iss>>s;)   // syntax for istringstream
         v.push_back(s);
     int n=v.size();
     int a[n];//for random generations
@@ -35,12 +27,11 @@ int main()
     }
     i=0;
     cout<<"\noriginal Message :\n";
-    fr(0,n)
+    for(i=0;i<n;i++)
     {
         cout<<i<<v[i]<<" ";
     }
     cout<<endl;
 
-    //print(a,n);
 
 }
