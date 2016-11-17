9a6bfb497b2ff0694a9039a43635c6107de26105
pkdhirasaria <pkdhirasaria@gmail.com>
2016-11-18 02:36:04 +0530

    Create 3.tcl

---
+++ Part-A/3/3.tcl
@@ -0,0 +1,107 @@
+# This script is created by NSG2 beta1
+# <http://wushoupong.googlepages.com/nsg>
+
+#===================================
+#     Simulation parameters setup
+#===================================
+set val(stop)   10.0                         ;# time of simulation end
+
+#===================================
+#        Initialization        
+#===================================
+#Create a ns simulator
+set ns [new Simulator]
+
+#Open the NS trace file
+set tracefile [open out.tr w]
+$ns trace-all $tracefile
+
+#Open the NAM trace file
+set namfile [open out.nam w]
+$ns namtrace-all $namfile
+
+#===================================
+#        Nodes Definition        
+#===================================
+#Create 6 nodes
+set n0 [$ns node]
+set n1 [$ns node]
+set n2 [$ns node]
+set n3 [$ns node]
+set n4 [$ns node]
+set n5 [$ns node]
+
+#===================================
+#        Links Definition        
+#===================================
+#Createlinks between nodes
+$ns duplex-link $n0 $n1 1.0Mb 10ms DropTail
+$ns queue-limit $n0 $n1 5
+$ns duplex-link $n2 $n1 1.0Mb 10ms DropTail
+$ns queue-limit $n2 $n1 5
+$ns duplex-link $n1 $n4 0.2Mb 10ms DropTail
+$ns queue-limit $n1 $n4 5
+$ns duplex-link $n4 $n5 1.0Mb 10ms DropTail
+$ns queue-limit $n4 $n5 5
+$ns duplex-link $n4 $n3 1.0Mb 10ms DropTail
+$ns queue-limit $n4 $n3 5
+
+#Give node position (for NAM)
+$ns duplex-link-op $n0 $n1 orient right-down
+$ns duplex-link-op $n2 $n1 orient right-up
+$ns duplex-link-op $n1 $n4 orient right
+$ns duplex-link-op $n4 $n5 orient right-up
+$ns duplex-link-op $n4 $n3 orient right-down
+
+#===================================
+#        Agents Definition        
+#===================================
+#Setup a TCP connection
+set tcp0 [new Agent/TCP]
+$ns attach-agent $n0 $tcp0
+set sink2 [new Agent/TCPSink]
+$ns attach-agent $n5 $sink2
+$ns connect $tcp0 $sink2
+$tcp0 set packetSize_ 1500
+
+#Setup a TCP connection
+set tcp1 [new Agent/TCP]
+$ns attach-agent $n2 $tcp1
+set sink3 [new Agent/TCPSink]
+$ns attach-agent $n3 $sink3
+$ns connect $tcp1 $sink3
+$tcp1 set packetSize_ 1500
+
+
+#===================================
+#        Applications Definition        
+#===================================
+#Setup a FTP Application over TCP connection
+set ftp0 [new Application/FTP]
+$ftp0 attach-agent $tcp0
+$ns at 1.0 "$ftp0 start"
+$ns at 5.0 "$ftp0 stop"
+
+#Setup a FTP Application over TCP connection
+set ftp1 [new Application/FTP]
+$ftp1 attach-agent $tcp1
+$ns at 1.0 "$ftp1 start"
+$ns at 5.0 "$ftp1 stop"
+
+
+#===================================
+#        Termination        
+#===================================
+#Define a 'finish' procedure
+proc finish {} {
+    global ns tracefile namfile
+    $ns flush-trace
+    close $tracefile
+    close $namfile
+    exec nam out.nam &
+    exit 0
+}
+$ns at $val(stop) "$ns nam-end-wireless $val(stop)"
+$ns at $val(stop) "finish"
+$ns at $val(stop) "puts \"done\" ; $ns halt"
+$ns run
